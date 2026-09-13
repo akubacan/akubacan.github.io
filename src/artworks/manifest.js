80ba91a6f4f223a6886bf636a873eacf414cd72d
akubacan <70151562+akubacan@users.noreply.github.com>
2026-09-13 09:53:40 +0700

    Add files via upload

---
+++ src/artworks/manifest.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    "url": "https://res.cloudinary.com/wtes366z/image/upload/f_auto/q_auto/v1789257235/c6cntfch1mzf0hqilkwn.png",
+    "type": "image",
+    "title": "Mount Kambuno",
+    "artist": "Frans Snyders (Flemish, 1579-1657)",
+    "year": "1614",
+    "link": "https://www.artic.eduartworks/62042",
+    "width": 1920,
+    "height": 1017
+  }
+];
