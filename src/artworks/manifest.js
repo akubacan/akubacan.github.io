e8ffb3c0fd135518921a4eacb62c62a9ab7635d7
akubacan <70151562+akubacan@users.noreply.github.com>
2026-09-14 13:52:32 +0700

    Update manifest.js

--- src/artworks/manifest.js
+++ src/artworks/manifest.js
@@ -8,5 +8,15 @@ export default [
     "link": "https://www.artic.eduartworks/62042",
     "width": 1920,
     "height": 1017
+  },
+  {
+    "url": "https://res.cloudinary.com/wtes366z/image/upload/v1789368327/final.png",
+    "type": "image",
+    "title": "Mount Gamalama",
+    "artist": "Frans Snyders (Flemish, 1579-1657)",
+    "year": "1614",
+    "link": "https://www.artic.eduartworks/62042",
+    "width": 1920,
+    "height": 1017
   }
 ];
